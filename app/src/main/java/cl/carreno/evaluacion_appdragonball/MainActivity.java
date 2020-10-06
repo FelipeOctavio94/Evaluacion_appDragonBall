b844b5f8044776f047f56569df5de4783a007991
felipe <felipe.carreno@hotmail.com>
2020-10-06 17:32:55 -0300

    API primera parte

--- app/src/main/java/cl/carreno/evaluacion_appdragonball/MainActivity.java
+++ app/src/main/java/cl/carreno/evaluacion_appdragonball/MainActivity.java
@@ -4,12 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import cz.msebera.android.httpclient.Header;
+
 public class MainActivity extends AppCompatActivity {
 
+    private static final String URL="https://dragon-ball-api.herokuapp.com/api/character/";
     EditText txt;
 
     @Override
@@ -17,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         txt = findViewById(R.id.user);
+        loadinformation();
     }
 
     public void Login(View view) {
@@ -26,5 +38,36 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = new Intent(this, CoffeeActivity.class);
             startActivity(intent);
         }
+    }
+
+    public void loadinformation(){
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get(URL, new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                String data = new String(responseBody);
+                Log.d("INFO",data);
+                processCharacter(data);
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+
+            }
+        });
+    }
+
+    public void processCharacter(String data) {
+        try {
+            JSONObject root = new JSONObject(data);
+            JSONArray character = root.getJSONArray("character");
+        }catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public void Creator(View view) {
+        Toast.makeText(this,"Creadores: Cristobal Canales, Felipe Carreño",Toast.LENGTH_LONG).show();
     }
 }
