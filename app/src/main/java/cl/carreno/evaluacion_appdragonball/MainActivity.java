3570da0eb63a91f4763fecadff4c45acf00d8023
felipe <felipe.carreno@hotmail.com>
2020-10-06 16:48:54 -0300

    Login y validacion

--- app/src/main/java/cl/carreno/evaluacion_appdragonball/MainActivity.java
+++ app/src/main/java/cl/carreno/evaluacion_appdragonball/MainActivity.java
@@ -2,13 +2,29 @@ package cl.carreno.evaluacion_appdragonball;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText txt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txt = findViewById(R.id.user);
+    }
+
+    public void Login(View view) {
+        if (txt.getText().toString().isEmpty()){
+            Toast.makeText(this,"Please complete your user",Toast.LENGTH_LONG).show();
+        }else {
+            Intent intent = new Intent(this, CoffeeActivity.class);
+            startActivity(intent);
+        }
     }
 }
